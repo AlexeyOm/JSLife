9c7a7ae1adb90aad9aac67055e66727e9aac3b15
Alexey Omelchenko <alexey.omelchenko@gmail.com>
2015-06-04 00:39:57 +0300

    Add 4 neighbour search

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 $(document).ready(function () {
-    var life = {
-        cellCount : 100,
-        rowWidth : 10,
-        colHeight : 10,
-        cells : new Array (100)
-    };
     
-    
-    life.getNeighbour = function (cellNum, direction) {
-        
-        
-        if(this.cells[cellNum] === 1) {
-            return 1;
-        }
-    };
     
     
     $("td").click(function () { 
@@ -31,7 +17,8 @@ $(document).ready(function () {
             }
         });
         //alert (life.cells);
-        alert (life.getNeighbour(2));
+        alert (life.getNeighbour($("input").val()));
+        //alert (life.getNeighbour(92));
         /*
         for (i = 0; i < life.cells.length; i++) {
             if
@@ -44,18 +31,44 @@ $(document).ready(function () {
 
 
 
-/*
- * [
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0],
-            [0,0,0,0,0,0,0,0,0,0]
-        ]
+var life = {
+        cellCount : 100,
+        rowWidth : 10,
+        colHeight : 10,
+        cells : new Array (100)
+    };
+    
+    
+    life.getNeighbour = function (cellNumString) {
+        var cellNum = parseInt(cellNumString, 10);
+        var neighbourCount = 0;
+        var nText = "";
+        //левый сосед
+        if(cellNum % this.rowWidth == 0) {
+            if(this.cells[cellNum + this.rowWidth - 1] == 1) {neighbourCount ++; nText += "left ";}
+        } else {
+            if(this.cells[cellNum - 1] == 1) {neighbourCount ++; nText += "left ";}
+        }
         
-        */
+        //правый сосед
+        if((cellNum +1)% this.rowWidth == 0) {
+            if(this.cells[cellNum - this.rowWidth + 1] == 1) {neighbourCount ++; nText += "right "; }
+        } else {
+            if(this.cells[cellNum + 1] == 1) {neighbourCount ++; nText += "right ";}
+        }
+        
+        //нижний сосед
+        if((cellNum + this.rowWidth) >= this.cellCount) {
+            if(this.cells[cellNum % this.rowWidth] == 1) {neighbourCount ++; nText += "lower " ;}
+        } else {
+            if(this.cells[cellNum + this.rowWidth] == 1) {neighbourCount ++; nText += "lower ";}
+        }
+        
+        //верхний сосед
+        if(cellNum - this.rowWidth < 0) {
+            if(this.cells[this.cellCount - this.rowWidth + cellNum] == 1) {neighbourCount ++; nText += "upper ";}
+        } else {
+            if(this.cells[cellNum - this.rowWidth] == 1) {neighbourCount ++; nText += "upper ";}
+        }
+            return nText;
+        };
